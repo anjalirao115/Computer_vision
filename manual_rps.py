9f1fa2489109cc93d8f531037881fd4abc52ba37
anjali <anjalirao115@gmail.com>
2022-09-22 10:42:42 +0100

    Milestone2: task 4: done

--- manual_rps.py
+++ manual_rps.py
@@ -9,29 +9,31 @@ def get_computer_choice(word_list) -> str:
 def get_user_choice() -> str :
     u_choice = input("rock, paper or scissors?:")
     return u_choice
-
+  
 def get_winner(computer_choice, user_choice):
 
-    computer_choice = get_computer_choice(words)
-    user_choice = get_user_choice()
+    #computer_choice = get_computer_choice(words)
+    #user_choice = get_user_choice()
 
     print(f"Computer chose {computer_choice}") 
 
     if computer_choice == 'rock' and user_choice == 'paper':
-        print('user wins')
+        print('You win')
     elif computer_choice == 'paper' and user_choice=='rock':
-        print('computer wins')    
+        print('Computer wins')    
     elif computer_choice == 'rock' and user_choice=='scissors':
-        print('computer wins')
+        print('Computer wins')
     elif computer_choice == 'scissors' and user_choice=='rock':
-        print('user wins')    
+        print('You win')    
     elif computer_choice == 'paper' and user_choice=='scissors':
-        print('user wins')
+        print('You win')
     elif computer_choice == 'scissors' and user_choice=='paper':
-        print('computer wins')   
+        print('Computer wins')   
     elif computer_choice == user_choice:
         print("It's a draw !!")     
-       
+
+c_choice = get_computer_choice(words)      
+u_choice = get_user_choice()
 get_winner(c_choice, u_choice)
 
 
