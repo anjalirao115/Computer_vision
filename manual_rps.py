483a6a1e439b6520145e8014e0cb5aea4a8f53ae
anjali <anjalirao115@gmail.com>
2022-09-22 10:06:19 +0100

    Milestone2: task 4: done

---
+++ manual_rps.py
@@ -0,0 +1,40 @@
+import random
+
+words = ['rock', 'rock', 'rock']
+
+def get_computer_choice(word_list) -> str:
+    c_choice = random.choice(word_list)
+    return c_choice
+
+c_choice = get_computer_choice(words)  
+#print(c_choice)  
+
+def get_user_choice() -> str :
+    u_choice = input("rock, paper or scissors?:")
+    return u_choice
+
+#u_choice = get_user_choice()
+#print(u_choice)
+
+def get_winner(computer_choice, user_choice):
+    #print(f"Computer chose {c_choice}")
+    print(f"Computer chose {get_computer_choice(words)}")
+
+    user_choice = get_user_choice()
+
+    if get_computer_choice(words) == 'rock' and user_choice == 'paper':
+        print('user wins')
+    #elif computer_choice == 'paper' and user_choice=='rock':
+        #print('computer wins')    
+    #elif computer_choice == 'rock' and user_choice=='scissors':
+        #print('computer wins')
+    #elif computer_choice == 'scissors' and user_choice=='rock':
+        #print('user wins')    
+    #elif computer_choice == 'paper' and user_choice=='scissors':
+        #print('user wins')
+    #elif computer_choice == 'scissors' and user_choice=='paper':
+        #print('computer wins')   
+    #elif computer_choice == user_choice:
+        #print("It's a draw !!")     
+       
+#get_winner(c_choice, u_choice)
