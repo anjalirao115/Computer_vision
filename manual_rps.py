7e1360ccceec22c389f18d5e692c768885fdec9a
anjali <anjalirao115@gmail.com>
2022-09-22 10:32:14 +0100

    Milestone2: task 4: done

--- manual_rps.py
+++ manual_rps.py
@@ -1,41 +1,36 @@
 import random
 
-words = ['rock', 'rock', 'rock']
+words = ['rock', 'paper', 'scissors']
 
 def get_computer_choice(word_list) -> str:
     computer_selection = random.choice(word_list)
-    return computer_selection
-
-c_choice = get_computer_choice(words)  
-#print(c_choice)  
+    return computer_selection 
 
 def get_user_choice() -> str :
     u_choice = input("rock, paper or scissors?:")
     return u_choice
 
-u_choice = get_user_choice()
-#print(u_choice)
-
 def get_winner(computer_choice, user_choice):
-    #print(f"Computer chose {c_choice}")
-    print(f"Computer chose {get_computer_choice(words)}")
 
-    #user_choice = get_user_choice()
+    computer_choice = get_computer_choice(words)
+    user_choice = get_user_choice()
 
-    if get_computer_choice(words) == 'rock' and u_choice == 'paper':
+    print(f"Computer chose {computer_choice}") 
+
+    if computer_choice == 'rock' and user_choice == 'paper':
         print('user wins')
-    #elif computer_choice == 'paper' and user_choice=='rock':
-        #print('computer wins')    
-    #elif computer_choice == 'rock' and user_choice=='scissors':
-        #print('computer wins')
-    #elif computer_choice == 'scissors' and user_choice=='rock':
-        #print('user wins')    
-    #elif computer_choice == 'paper' and user_choice=='scissors':
-        #print('user wins')
-    #elif computer_choice == 'scissors' and user_choice=='paper':
-        #print('computer wins')   
-    #elif computer_choice == user_choice:
-        #print("It's a draw !!")     
+    elif computer_choice == 'paper' and user_choice=='rock':
+        print('computer wins')    
+    elif computer_choice == 'rock' and user_choice=='scissors':
+        print('computer wins')
+    elif computer_choice == 'scissors' and user_choice=='rock':
+        print('user wins')    
+    elif computer_choice == 'paper' and user_choice=='scissors':
+        print('user wins')
+    elif computer_choice == 'scissors' and user_choice=='paper':
+        print('computer wins')   
+    elif computer_choice == user_choice:
+        print("It's a draw !!")     
        
 get_winner(c_choice, u_choice)
 
